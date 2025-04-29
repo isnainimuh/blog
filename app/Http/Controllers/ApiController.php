5301d886c70613c175422bb68c8b06b6bfcbdfdd
isnainimuh <imuhandhis@gmail.com>
2025-04-29 07:22:33 +0700

    pertemuan 3 sampai 5

---
+++ app/Http/Controllers/ApiController.php
@@ -0,0 +1,163 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use \Tymon\JWTAuth\Facades\JWTAuth;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\DB;
+use App\Models\Member;
+use Tymon\JWTAuth\Exceptions\TokenExpiredException;
+use Tymon\JWTAuth\Exceptions\TokenInvalidException;
+
+class ApiController extends Controller
+{
+    //
+    public function register(Request $request) {
+        // Pastikan user sudah login (memiliki token)
+        try {
+            $user = JWTAuth::parseToken()->authenticate();
+        } catch (TokenExpiredException $e) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Token has expired, please login again'
+            ], Response::HTTP_UNAUTHORIZED);
+        } catch (TokenInvalidException $e) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Invalid token, please provide a valid token'
+            ], Response::HTTP_UNAUTHORIZED);
+        } catch (JWTException $e) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Token not provided or is incorrect'
+            ], Response::HTTP_UNAUTHORIZED);
+        }
+
+        if (!$user) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Invalid token or user not authenticated'
+            ], Response::HTTP_UNAUTHORIZED);
+        }
+
+        // Validasi data input
+        $data = $request->only('name', 'email', 'password');
+        $validator = Validator::make($data, [
+            'name' => 'required|string',
+            'email' => 'required|email|unique:users,email,' . $user->id,
+            'password' => 'required|string|min:6|max:50'
+        ]);
+
+        // Jika validasi gagal
+        if ($validator->fails()) {
+            return response()->json(['error' => $validator->messages()], 400);
+        }
+
+        // Update user Guest menjadi user dengan email dan password yang valid
+        $user->update([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        return response()->json([
+            'success' => true,
+            'message' => 'User registered successfully',
+            'data' => $user
+        ], Response::HTTP_OK);
+    }
+
+    public function authenticate(Request $request) {
+        $credentials = $request->only('email', 'password');
+
+        //valid credential
+        $validator = Validator::make($credentials, [
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        //Send failed response if request is not valid
+        if ($validator->fails()) {
+            return response()->json([ 'success' => false,'message' => $validator->messages()], 200);
+        }
+
+
+        try {
+            if (!$token = JWTAuth::attempt($credentials)) {
+                return response()->json([
+                    'success' => false,
+                    'message' => 'email atau password salah.',
+                ], 200);
+            }
+        } catch (JWTException $e) {
+            return $credentials;
+            return response()->json([
+                'success' => false,
+                'message' => 'Could not create token.',
+            ], 500);
+        }
+
+        // Check if user is verified
+        $user = auth()->user();
+
+        if (!$user->email_verified_at) {
+            // User is not verified
+            return response()->json([
+                'success' => false,
+                'message' => 'Silahkan verifikasi email anda terlebih dahulu.',
+            ], 200);
+        }
+
+        // Token created, return with success response and JWT token
+        return response()->json([
+            'success' => true,
+            'token' => $token,
+            'name' => $user->name,
+            'email' => $user->email,
+        ]);
+    }
+
+    public function logout(Request $request) {
+        //Request is validated, do logout
+        try {
+            $validator = Validator::make($request->only('token'), [
+                'token' => 'required'
+            ]);
+
+            $user = JWTAuth::parseToken()->authenticate();
+            if ($user) {
+
+                $user->update(['token_firebase' => null]);
+            }
+
+            //Send failed response if request is not valid
+            if ($validator->fails()) {
+                return response()->json(['error' => $validator->messages()], 200);
+            }
+
+
+
+            $token = new \Tymon\JWTAuth\Token($request->token);
+            \Tymon\JWTAuth\Facades\JWTAuth::manager()->invalidate($token, $forever = true);
+
+            return response()->json([
+                'success' => true,
+                'message' => 'User has been logged out'
+            ]);
+        } catch (JWTException $exception) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Sorry, user cannot be logged out'
+            ], 200);
+        }
+    }
+
+    public function get_user(Request $request) {
+
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ApiController;
+use App\Http\Controllers\Api\CountryController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +19,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('login', [ApiController::class, 'authenticate']);
+Route::post('logout', [ApiController::class, 'logout']);
+Route::post('register', [ApiController::class, 'register']);
+Route::apiResource('countries', CountryController::class);
